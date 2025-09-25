994d26111b21f6b6ea1eb3c208f39d0d188b7fc0
ceccav <ceciliacavallin@hotmail.se>
2025-09-26 00:03:23 +0200

    Added logic for class Item
    logic to add an item with owner email, description and unique ID

--- Item.cs
+++ Item.cs
@@ -1,3 +1,24 @@
 //name
 //description
 namespace App;
+
+public class Item
+{
+    private static int counter = 1;
+    public string OwnerEmail = "";
+
+    public string ItemToTrade = "";
+
+    public string Description;
+
+    public string ID;
+
+    public Item(string ownerEmail, string itemToTrade, string description)
+    {
+        OwnerEmail = ownerEmail;
+        ItemToTrade = itemToTrade;
+        Description = description;
+        ID = counter.ToString("D4");
+        counter++;
+    }
+}
