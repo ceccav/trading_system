ce25e319c3dc20a0ee975e1f2f9f6f1be7db9f16
ceccav <ceciliacavallin@hotmail.se>
2025-09-26 13:10:01 +0200

    Added comments
    Added comments to describe what everything in the Item.cs does.

--- Item.cs
+++ Item.cs
@@ -4,21 +4,28 @@ namespace App;
 
 public class Item
 {
-    private static int counter = 1;
-    public string OwnerEmail = "";
+    private static int counter = 1; //counter to give each item an uniqe ID
+    public string OwnerEmail = ""; // string to store ownerEmail
 
-    public string ItemToTrade = "";
+    public string ItemToTrade = ""; //string to hold the itam to trade
 
-    public string Description;
+    public string Description; //string to hold the description of the item
 
-    public string ID;
+    public string ID; //string for id
 
+
+    /// <summary>
+    /// Constructor to create an item object with wet owner, item to trade, description and unique ID
+    /// </summary>
+    /// <param name="ownerEmail">email to the owner of the item</param>
+    /// <param name="itemToTrade">the item the user wants to trade</param>
+    /// <param name="description">description of the item</param>
     public Item(string ownerEmail, string itemToTrade, string description)
     {
-        OwnerEmail = ownerEmail;
-        ItemToTrade = itemToTrade;
-        Description = description;
-        ID = counter.ToString("D4");
-        counter++;
+        OwnerEmail = ownerEmail; //sets owner email
+        ItemToTrade = itemToTrade; //sets item to trade
+        Description = description; //sets a description
+        ID = counter.ToString("D4"); //creates a unique id for example 0001, 0002
+        counter++; // for each created object the counter adds one to the count.
     }
 }
